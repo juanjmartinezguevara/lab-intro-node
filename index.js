06a119f40739f4364cae17b7df93f9d101cef337
Juan Martinez Guevara <JuanJMtzGvra@Juans-Macbook.attlocal.net>
2021-02-23 12:16:06 -0600

    Complete iterations 1, 3-14

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("OutofBounds"); //Are all error messages initiated in this way?
+    }
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+    return Math.max(...this.items); //When exactly we should use ... before an array again?
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+
+    return this.items.reduce((a, v) => a + v);
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.items.length;
+    //return this.items.reduce((a, v) => a + v, 0) / this.items.length          //Why is this giving me an error? Is it the double use of this.items?
+  }
 }
 
 module.exports = SortedList;
